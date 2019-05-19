9bf42b5177b1ac5794606e31dd7e7c9743828b50
Douglas Wade Goodall <douglas_goodall@mac.com>
2019-05-19 07:59:46 -0700

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+// hello.cpp
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x = 0;
+    x += 1;
+    x += 2;
+    printf("Content-type:\ttext/html\n\n");
+    printf("<p>Hello World!!\n");
+    x += 3;
+    return 0;
+}
